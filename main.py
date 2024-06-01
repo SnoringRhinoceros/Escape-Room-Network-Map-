f3a87a504b8cd7457689f5d017b35e39d27229f5
David <davidshuschool@gmail.com>
2024-06-01 15:41:04 -0500

    Changed screen size

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ pygame.init()
 submit_delay = 10000  # milliseconds
 
 # Screen dimensions
-screen_width = 1000
-screen_height = 1000
+screen_width = 1024
+screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Drag and Drop Text")
 
